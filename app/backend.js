56d8aabc7364eef6672476071d92c6dd0965b54a
Tyler Speakman <tyler.speakman@gmail.com>
2013-09-22 13:27:30 -0400

    - Fixed git -- removed bad .gitignore

--- app/backend.js
+++ app/backend.js
@@ -1,11 +1,6 @@
 var requirejs = require('requirejs');
 
-requirejs.config({
-	//Pass the top-level main.js/index.js require
-	//function to requirejs so that node modules
-	//are loaded relative to the top-level JS file.
-	nodeRequire: require
-});
+requirejs.config({nodeRequire: require});
 
 requirejs(['backend/server', 'backend/router'], function(server, router) {
 	'use strict';
