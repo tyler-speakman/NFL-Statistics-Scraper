04c4028b98937a91d6874c833a3ce3ae980415c9
Tyler Speakman <tyler.speakman@gmail.com>
2013-09-23 19:33:29 -0400

    - More Heroku debugging

--- app/backend.js
+++ app/backend.js
@@ -2,9 +2,15 @@ var requirejs = require('requirejs');
 
 requirejs.config({
 	nodeRequire: require,
+	baseUrl: '../',
 	paths: {
-	    app: '../app'
-	}
+	    // app: '../app',
+	    // async: '../node_modules/async',
+	    // connect: '../node_modules/connect',
+	    // async: '../node_modules/gzippo',
+	    // async: '../node_modules/lodash',
+	    // async: '../node_modules/lru-cache',
+	}	
 });
 
 requirejs(['backend/server', 'backend/router'], function(server, router) {
