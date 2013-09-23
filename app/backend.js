4b00dd0a4d82567411adaa720772f37bf8b7f9f2
Tyler Speakman <tyler.speakman@gmail.com>
2013-09-23 19:27:33 -0400

    - More Heroku debugging

--- app/backend.js
+++ app/backend.js
@@ -1,6 +1,11 @@
 var requirejs = require('requirejs');
 
-requirejs.config({nodeRequire: require});
+requirejs.config({
+	nodeRequire: require,
+	paths: {
+	    app: '../app'
+	}
+});
 
 requirejs(['backend/server', 'backend/router'], function(server, router) {
 	'use strict';
